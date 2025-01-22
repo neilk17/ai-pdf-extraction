a3c0bb28fa5b82b1eed1de47517521edd92dd843
Neil Kanakia <kanakianeil@gmail.com>
2025-01-22 11:44:09 +0000

    simple example for pdf text extraction

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
+import { anthropic } from "@ai-sdk/anthropic";
+import { z } from "zod"
+import path from "path";
+import { readFileSync } from "fs";
+import dotenv from "dotenv";
+import { generateObject } from "ai";
 
-const model = openai("gpt-4o-mini");
+dotenv.config();
 
-export const answerMyQuestion = async () => {
-    const { text } = await generateText({
-        model,
-        prompt: "Generate 5 words",
-        system: "You are generateing random words",
-        output: "array",
-    });
+const model = anthropic("claude-3-5-sonnet-latest");
 
-  return text;
-};
+const schema = z.object({
+  invNum: z.string().describe("Pro. Inv. No."),
+  consignee: z.string().describe("Name of the consignee"),
+  terms: z.string().describe("Payment terms and delivery"),
+  descriptionOfGoods: z.string().describe("Description of goods")
+})
 
-const answer = await answerMyQuestion();
 
-console.log(answer);
+async function extractTextFromPdf(invoicePath) {
+  const { object } = await generateObject({
+    model,
+    system: `You will receive an invoice. ` +
+    `Please extract the data from the invoice.`, 
+    schema,
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "file",
+            data: readFileSync(invoicePath),
+            mimeType: "application/pdf",
+          },
+        ],
+      },
+    ],
+  })
+
+  return object;
+}
+
+const result = await extractTextFromPdf(
+  path.join(import.meta.dirname, './invoice.pdf')
+);
+
+console.dir(result, {depth: null});
